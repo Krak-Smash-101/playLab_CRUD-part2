ee6799e334ceb9ea14fbbdd7771cb383845d08ab
Krak-Smash-101 <X00132084@gmail.com>
2016-11-25 16:57:20 +0000

    CRUD Part2Commit

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -17,21 +17,72 @@ import models.*;
 
 public class HomeController extends Controller {
 
-    public Result index(String name) {
+    //Declare a private form factory
+    private FormFactory formFactory;
+
+    //Inject an instance of this into the Controller via the Constructor
+    @Inject
+    public HomeController(FormFactory f) {
+        this.formFactory = f;
+    }
+
+    public Result index(String name)
+    {
         return ok(index.render("Welcome to the Home page", name));
     }
 
-    public Result about() {
+    public Result about()
+    {
         return ok(about.render());
     }
 
     public Result products() {
-
         // Get list of all categories in ascending order
         List<Product> productsList = Product.findAll();
         return ok(products.render(productsList));
     }
 
+    public Result addProduct() {
+        //Wrap Product class in a FormFactory instance
+        Form<Product> addProductForm = formFactory.form(Product.class);
+
+        //Pass the form object
+        return ok(addProduct.render(addProductForm));
+    }
+
+    public Result addProductSubmit() {
+        //Bind the values submitted to form object based on Product
+        Form<Product> newProductForm = formFactory.form(Product.class).bindFromRequest();
+
+        //Check for errors
+        if(newProductForm.hasErrors()) {
+            return badRequest(addProduct.render(newProductForm));
+        }
+
+        //Extract the product from the forms object
+        Product newProduct = newProductForm.get();
+
+        //save to DB VIA ebean
+        newProduct.save();
+
+        //Set success message in tem flash
+        flash("success", "Product " + newProduct.getName() + " has been created");
+
+        //Redirect to the admin home
+        return redirect(controllers.routes.HomeController.products());
+    }
+
+    //Delete Product by ID
+    public Result deleteProduct(Long id){
+
+        //Find Prod by ID
+        Product.find.ref(id).delete();
+
+        //Add message to flash
+        flash("success", "Product has been deleted");
+
+        //Re-direct to Products
+        return redirect(routes.HomeController.products());
+    }
+
 }
-
-
